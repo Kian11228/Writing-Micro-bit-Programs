3859ef15fad75e659e57a8a800c2b9b1f2f044f8
Kian11228 <104364945+Kian11228@users.noreply.github.com>
2022-05-10 09:00:33 -0400

    Create medium_box.py
     see how a medium size box can be made by lighting up 8 pixels in the middle of the micro:bit module’s display.

---
+++ medium_box.py
@@ -0,0 +1,12 @@
+# medium_box.py
+
+from microbit import *
+
+display.set_pixel(1, 1, 9)
+display.set_pixel(1, 2, 9)
+display.set_pixel(1, 3, 9)
+display.set_pixel(2, 1, 9)
+display.set_pixel(2, 3, 9)
+display.set_pixel(3, 1, 9)
+display.set_pixel(3, 2, 9)
+display.set_pixel(3, 3, 9)
